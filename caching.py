7195c352ef2ca981eaa1ae56eb43b2750476d193
Egor Ibragimov <114207075+egoribragimov@users.noreply.github.com>
2024-03-20 06:09:19 +0500

    кэширующий декоратор
    функция принимает функцию и сохраняет значения

--- caching.py
+++ caching.py
@@ -5,3 +5,15 @@ def caching(function):
 			computed_results[args] = function(*args)
 		return computed_results[args]
 	return new_function
+
+
+#специально неудачная реализация 
+@caching
+def fib(n):
+	if n == 0 or n == 1:
+		return 1
+	return fib(n-1) + fib(n-2)
+
+
+print(fib(100))#79 ms
+print(fib(100))#при повторном вызове мнгновенно получаем рез
